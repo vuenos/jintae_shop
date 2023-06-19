649bdf090743a7781527580c08993c0302c1af4a
jintae kim <vuenos@naver.com>
2023-06-19 11:00:38 +0900

    API set

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ if (process.env.NODE_ENV === 'production') {
   )
 } else {
   app.get('/', (req, res) => {
-    res.send('API is running....')
+    res.send(`<h1>Running Works APIs... 🚀</h1>`)
   })
 }
 
